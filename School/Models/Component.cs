23e581ac14b641cb20f828fd04afa36f03470c99
Емельдяев Антон <aemelia2004@gmail.com>
2025-05-22 22:30:11 +0500

    Плдключил бд для работы с проектом

---
+++ School/Models/Component.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School;
+
+public partial class Component
+{
+    public int ComponentId { get; set; }
+
+    public string ComponentName { get; set; } = null!;
+
+    public string? ComponentDescription { get; set; }
+
+    public DateOnly ManufactureDate { get; set; }
+
+    public virtual ICollection<WarehouseStock> WarehouseStocks { get; set; } = new List<WarehouseStock>();
+}
---
+++ School/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School;
+
+public partial class Supplier
+{
+    public int SupplierId { get; set; }
+
+    public string SupplierName { get; set; } = null!;
+
+    public string? ContactInfo { get; set; }
+
+    public virtual ICollection<Delivery> Deliveries { get; set; } = new List<Delivery>();
+}
